96fc00e36d4bbefa1f071bf7c225257f239bd2e1
Nick <nikolpaladiy@gmail.com>
2023-08-15 20:03:49 -0400

    feat add: two components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+// must return JSX
 function App() {
-  return (
-    <div className="App">
+    return (
+        <div>
+            This is APP component
+        </div>
+    );
+}
+
+function Rating() {
+    return (
+        <div>
+            <div> star</div>
+            <div> star</div>
+            <div> star</div>
+            <div> star</div>
+            <div> star</div>
+        </div>
+    )
+}
+
+function Accordion() {
+    return <div>
+        <h3> Menu</h3>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
     </div>
-  );
 }
 
 export default App;
