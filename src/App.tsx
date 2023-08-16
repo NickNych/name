460c8c76345c9513be6d6494d0f563a0a3c1109f
Nick <nikolpaladiy@gmail.com>
2023-08-15 22:59:13 -0400

    feat: finish components part 2 and homework

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,73 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import {Debugger} from "inspector";
+// import {Debugger} from "inspector";
+
 
 
 // must return JSX
 function App() {
+    console.log("App is rendering")
     return (
         <div>
-            This is APP component
+            <AppTitle/>
             <Rating/>
             <Accordion/>
+            <Rating/>
         </div>
-    );
+
+    )
 }
 
+function AppTitle() {
+    console.log("AppTitle is rendering")
+
+    return <>This is APP component</>
+}
+
+
 function Star() {
+    console.log("Star is rendering")
+
     return <div>
-        star</div>
+        star
+    </div>
 }
 
 function Rating() {
-    debugger
+    console.log("Rating is rendering")
     return (
-
-        <div>
+        <>
             <Star/>
             <Star/>
             <Star/>
             <Star/>
             <Star/>
-        </div>
+        </>
     )
+}
+
+function AccordionTitle() {
+    console.log("App is rendering")
+    return  <h3>Menu</h3>;
+}
+
+function AccordionBody() {
+    console.log("App is rendering")
+    return  <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>;
 }
 
 function Accordion() {
     return <div>
-        <h3>Menu</h3>
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-        </ul>
+        <AccordionTitle/>
+        <AccordionBody/>
     </div>
 }
+
+
 
 export default App;
