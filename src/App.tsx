1813d18f367aec69def47ae6189a3d8452c168f2
Nick <nikolpaladiy@gmail.com>
2023-08-15 20:49:53 -0400

    feat: finish lesson components part 1 with home work

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,42 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import {Debugger} from "inspector";
+
 
 // must return JSX
 function App() {
     return (
         <div>
             This is APP component
+            <Rating/>
+            <Accordion/>
         </div>
     );
 }
 
+function Star() {
+    return <div>
+        star</div>
+}
+
 function Rating() {
+    debugger
     return (
+
         <div>
-            <div> star</div>
-            <div> star</div>
-            <div> star</div>
-            <div> star</div>
-            <div> star</div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
         </div>
     )
 }
 
 function Accordion() {
     return <div>
-        <h3> Menu</h3>
+        <h3>Menu</h3>
         <ul>
             <li>1</li>
             <li>2</li>
